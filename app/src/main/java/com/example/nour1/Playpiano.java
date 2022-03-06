c69b60c3d61636f09e1ecc8b04c9e7bd1048eb9f
noor264 <noor.othman264@gmail.com>
2022-03-06 21:20:20 +0200

    ولا اشي زابط

--- app/src/main/java/com/example/nour1/Playpiano.java
+++ app/src/main/java/com/example/nour1/Playpiano.java
@@ -1,18 +1,36 @@
 package com.example.nour1;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Playpiano extends AppCompatActivity {
 
     AudioPiano all;
+    Button c;
+    Button d;
+    Button db;
+    Button e;
+    Button eb;
+    Button f;
+    Button g;
+    Button gb;
+    Button a;
+    Button ab;
+    Button b;
+    Button bb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playpiano);
+        connectComponents();
+    }
+
+    private void connectComponents() {
         all = AudioPiano.getInstance(this);
+
     }
 
 
