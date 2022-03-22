425e14f205c26cee7c9c890eb95e08e94ce2b8ee
Noor Othman <noor.othman264@gmail.com>
2022-03-22 12:03:31 +0200

    layerlist btns

--- app/src/main/java/com/example/nour1/Playpiano.java
+++ app/src/main/java/com/example/nour1/Playpiano.java
@@ -1,12 +1,15 @@
 package com.example.nour1;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Playpiano extends AppCompatActivity {
 
+    MediaPlayer player;
     AudioPiano all;
     Button c;
     Button d;
@@ -30,8 +33,79 @@ public class Playpiano extends AppCompatActivity {
 
     private void connectComponents() {
         all = AudioPiano.getInstance(this);
-
     }
 
+    public void playa(View view) {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.a4);
+        }
+        player.start();
+    }
+    public void playab(View view) {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.ab4);
+        }
+        player.start();
+    }
+    public void playb(View view) {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.b4);
+        }
+        player.start();
+    }
+    public void playbb(View view) {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.bb4);
+        }
+        player.start();
+    }
+    public void playc(View view) {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.c4);
+        }
+        player.start();
+    }
+    public void playd(View view) {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.d4);
+        }
+        player.start();
+    }
+    public void playdb(View view) {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.db4);
+        }
+        player.start();
+    }
+    public void playe(View view) {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.e4);
+        }
+        player.start();
+    }
+    public void playeb(View view) {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.eb4);
+        }
+        player.start();
+    }
+    public void playf(View view) {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.f4);
+        }
+        player.start();
+    }
+    public void playg(View view) {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.g4);
+        }
+        player.start();
+    }
+    public void playgb(View view) {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.gb4);
+        }
+        player.start();
+    }
 
 }
---
+++ app/src/main/java/com/example/nour1/TestCanvasActivity.java
@@ -0,0 +1,15 @@
+package com.example.nour1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TestCanvasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_canvas);
+    }
+}
+//https://www.ssaurel.com/blog/creating-a-virtual-piano-application-for-android/
